c4899a69d8a7f92f8e983f51cfe73b2abd7e65c0
Muhammad Arafat <97059953+Arafat05nov@users.noreply.github.com>
2023-06-19 01:25:36 +0500

    Create Create_colored_ geometrical_shapes.py

---
+++ Create_colored_ geometrical_shapes.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+import random
+
+timmy = Turtle()
+
+def change_color():
+    R = random.random()
+    B = random.random()
+    G = random.random()
+
+    timmy.color(R, G, B)
+
+sides = 3
+timmy.pensize(10)
+for i in range(7):
+    change_color()
+    for side in range(sides):
+        timmy.forward(100)
+        timmy.right(360/sides)
+    sides += 1
